cd02c0b4998ff5c65e9aecbcf01d231dd08e8840
Devansh-407 <devansh.codee@gmail.com>
2025-06-21 13:27:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -17,3 +17,20 @@
 
 // let name = "Devansh Gupta";
 // console.log(name.startsWith("D"));  // true
+
+// let age = 20;
+// if (age >= 18) {
+//     console.log("You are an adult");
+// } else {
+//     console.log("You are a minor");
+// }
+
+// let fruits = ["apple", "banana", "mango"];
+// fruits.push("orange");
+// console.log(fruits);
+// console.log("First fruit is: " + fruits[0]);
+
+// let colors = ["red", "green", "blue"];
+// for (let i = 0; i < colors.length; i++) {
+//     console.log("Color: " + colors[i]);
+// }
