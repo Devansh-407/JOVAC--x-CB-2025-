a6079a947028a0d93d7a679f8f7b312acba8b449
Devansh-407 <devansh.codee@gmail.com>
2025-06-21 13:24:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//let name = "Devansh Gupta";
+//console.log("My name is " + name);
+
+
+//let name = "Devansh Gupta";
+//console.log(name.toUpperCase());
+
+// let fullName = "Devansh Gupta";
+// let firstName = "Devansh";
+// console.log(fullName.includes(firstName))
+
+
+// let name = "Devansh Gupta";
+// for (let i = 0; i < name.length; i++) {
+//     console.log(name[i]);
+// }
+
+// let name = "Devansh Gupta";
+// console.log(name.startsWith("D"));  // true
